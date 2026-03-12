3afebab9898eedf6a15f2bf82653a0f44ecbd1ba
darynabar <daryna281106@gmail.com>
2026-03-12 12:59:49 +0200

    edit json

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'students-cache-v1';
+const CACHE_NAME = 'students-cache-v2';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -32,4 +32,15 @@ self.addEventListener('fetch', event => {
         return response || fetch(event.request);
       })
   );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(name => name !== CACHE_NAME)
+                          .map(name => caches.delete(name))
+            );
+        })
+    );
 });
